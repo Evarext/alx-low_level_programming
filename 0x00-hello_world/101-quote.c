4f22fae56eb79a981dc61ca50835106bf4feb3bf
evarext <uzogod65@gmail.com>
2022-09-08 20:06:01 -0700

    add "101-quote.c"

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main-main entry point
+ *
+ * description: intel file
+ *
+ * Return: 1 on succes
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

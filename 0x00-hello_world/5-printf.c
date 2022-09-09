36132330f3f3dfc096a1420f034ea0f6b2009455
evarext <uzogod65@gmail.com>
2022-09-08 18:46:02 -0700

    edit "5-printf.c"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  *
  * description: print using printf
  *
- * return: always 0 (succes)
+ * return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar,b but the outcome is a piece pf art,\n");
+	printf("with proper grammar, but the outcome is a piece pf art,\n");
 	return (0);
 }

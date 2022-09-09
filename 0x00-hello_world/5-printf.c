4a4534fbe06b9edc2515c38f959e6ae57609e1b8
evarext <uzogod65@gmail.com>
2022-09-08 18:41:05 -0700

    add "5-printf.c"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * description: print using printf
+ *
+ * return: always 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar,b but the outcome is a piece pf art,\n");
+	return (0);
+}

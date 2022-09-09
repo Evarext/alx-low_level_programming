ef8dd69b69626612a90715259892ec13ce83d419
evarext <uzogod65@gmail.com>
2022-09-08 19:31:26 -0700

    fixed betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  *
  * description: print using printf
  *
- * return: Always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece pf art,\n");
 	return (0);
 }

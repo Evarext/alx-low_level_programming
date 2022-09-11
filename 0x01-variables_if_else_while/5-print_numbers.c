93170464343f2e970f5cb011848e6af5f8715cbe
evarext <uzogod65@gmail.com>
2022-09-11 16:12:35 -0700

    add "5-print_numbers"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main-program that prints numbers from 1 to 10 in base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}

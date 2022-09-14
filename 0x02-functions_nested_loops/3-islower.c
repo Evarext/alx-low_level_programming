e6b5d49212c7ceaef0281b7b7794cd7cbc3be673
evarext <uzogod65@gmail.com>
2022-09-13 19:38:06 -0700

    fix typo in "3-islower" to the repo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * _islower-this program tells when a letter is a lower case
+ * @c: is the int that will be used for the argument of the function
  * it returns 1 if lowercase and returns o othewise
  * Return: 0
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}

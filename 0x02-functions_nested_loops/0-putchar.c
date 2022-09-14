36326bc99ee9d18151585a013a5c021d4d9ec56d
evarext <uzogod65@gmail.com>
2022-09-13 17:51:21 -0700

    edit file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,28 +2,13 @@
 #include <stdio.h>
 
 /**
- * _putchar-the function to be invoked
+ * main-the function to be invoked
  *
  * Return: 0
  */
 
-int _putchar(void)
-{
-	printf("_putchar");
-	return (0);
-}
-
-/**
- * main-main entry point of the code
- *
- * Return:0
- */
-
 int main(void)
 {
-	int c = _putchar();
-
-	putchar(c);
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }

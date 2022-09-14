0ea444de303c63d275f46d602a6e00c7d33aedaf
evarext <uzogod65@gmail.com>
2022-09-13 19:53:29 -0700

    add "4-isalpha.c"

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha-the function checks for letters, uppercase and lower case
+ *
+ * @c: c is used to pass argument to the function
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (( c >= 'a' && c <= 'z') || (c >= 'A' && c <='Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

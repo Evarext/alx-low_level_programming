29c4bde0e44eaff6e3ed7f3890f310aeef3d14e6
evarext <uzogod65@gmail.com>
2022-09-13 19:14:08 -0700

    add file "2-print_alphabet_x10.c"

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print-alphabet_x10-this program prints alphabets 10 times
+ * on a new line
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char n = 0;
+
+	while (n < 9)
+	{
+		char l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		n += 1;
+	}
+}

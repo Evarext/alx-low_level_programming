6c6e116ba4f5761911108fe56080cc7ffe9610d6
evarext <uzogod65@gmail.com>
2022-09-14 09:06:51 -0700

    add "10-add.c"

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add- function adds two numbers and returns the value
+ * Return: result of addition of two numbers
+ * @a: first number to be added
+ * @b: second number to be added
+ */
+
+int add(int a, int b)
+{
+	int result = "a + b";
+
+	return (result);
+}

d6f49415ef7cd863e961953ffc7e6967a64b4bd9
evarext <uzogod65@gmail.com>
2022-09-22 15:12:40 -0700

    remove whitespace in "100-rot13.c"

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * * rot13 - encodes a string into rot13
- * * @s: string to encode
+ * *rot13 - encodes a string into rot13
+ * *@s: string to encode
  * * Return: address of s
  */
 
@@ -11,7 +11,7 @@ char *rot13(char *s)
 	int i, j;
 	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	
+
 	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j < 52; j++)
